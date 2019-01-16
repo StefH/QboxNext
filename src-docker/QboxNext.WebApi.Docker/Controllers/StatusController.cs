e00e049cbfc247beac642f0a8fd12715c78b0e64
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-01-16 07:56:27 +0100

    Docker & version 20190115 (#3)
    * ServerTimeout
    
    * Add statuscontroller
    
    * readme

---
+++ src-docker/QboxNext.WebApi.Docker/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QboxNext.WebApi.Docker.Controllers
+{
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet("/")]
+        public string Get()
+        {
+            return "QboxNext.WebApi.Docker is running";
+        }
+    }
+}
---
+++ src-docker/QboxNext.WebApi.Docker/ReadMe.md
@@ -0,0 +1,4 @@
+﻿# Issues
+
+## Connect to Azure Storage Emulator from Docker
+This does not work. Use https://github.com/azure/azurite with command `azurite-table -l C:\Temp\_azurerite`.
