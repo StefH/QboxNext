9c6691c7cc94cbe4753c88f6cc0dc7b933abbf6d
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-01-23 13:01:21 +0100

    works!

--- src-server/QboxNext.Server.WebApi/Startup.cs
+++ src-server/QboxNext.Server.WebApi/Startup.cs
@@ -17,12 +17,18 @@ namespace QboxNext.Server.WebApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
