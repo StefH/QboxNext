c0cba2c39d6a3d466cea3b4a8ca65564c3373c14
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-02-05 10:45:12 +0100

    .

--- src-server/QboxNext.Server.Frontend/Startup.cs
+++ src-server/QboxNext.Server.Frontend/Startup.cs
@@ -41,6 +41,7 @@ namespace QboxNext.Server.Frontend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configure MVC AuthorizeFilter
             services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder()
