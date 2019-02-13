27aba87194de5ec0ead78e6528bb3a1e4eedf011
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-02-13 13:45:30 +0100

    set scopes

---
+++ src-server/QboxNext.Server.Frontend/Auth0/set scopes.js
@@ -0,0 +1,18 @@
+﻿function (user, context, callback) {
+    var req = context.request;
+
+    // Get requested scopes
+    var scopes = (req.query && req.query.scope) || (req.body && req.body.scope);
+
+    // Normalize scopes into an array
+    scopes = (scopes && scopes.split(" ")) || [];
+
+    // Restrict the access token scopes according to the current user
+    context.accessToken.scope = restrictScopes(user, scopes);
+
+    callback(null, user, context);
+
+    function restrictScopes(user, requested) {
+        return ['openid', 'read:data'];
+    }
+}
