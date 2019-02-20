8b28e56ff3c4344fadc70be1044b8599b435e2cc
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-02-20 15:57:08 +0100

    /version

--- src-server/QboxNext.Server.DataReceiver/Controllers/VersionController.cs
+++ src-server/QboxNext.Server.DataReceiver/Controllers/VersionController.cs
@@ -6,7 +6,7 @@ namespace QboxNext.Server.DataReceiver.Controllers
     [ApiController]
     public class VersionController : ControllerBase
     {
-        [HttpGet("/version")]
+        [HttpGet(@"/version/{productNumber:regex(^\d{{4}}-\d{{4}}-\d{{4}}$)}/{serialNumber:regex(^\d{{2}}-\d{{2}}-\d{{3}}-\d{{3}}$)}")]
         public ActionResult GetVersion()
         {
             return Ok(AssemblyUtils.GetVersion());
