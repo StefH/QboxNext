b9d95531825d3eaaa4edd1f19d454c401b5f417d
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-06-27 16:24:07 +0200

    wip

---
+++ src-server/QboxNext.Blazor/Program.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QboxNext.Blazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            //builder.Services.AddTransient(sp => new HttpClient
+            //{
+            //    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            //});
+
+            builder.Services.AddHttpClient("api")
+                .AddHttpMessageHandler(sp =>
+                {
+                    var handler = sp.GetService<AuthorizationMessageHandler>()
+                        .ConfigureHandler(
+                            authorizedUrls: new[] { "https://localhost:5002" },
+                            scopes: new[] { "openid profile" });
+
+                    return handler;
+                });
+
+            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("api"));
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
+                builder.Configuration.Bind("oidc", options.ProviderOptions);
+
+                // The callback url is : https://localhost:5001/authentication/login-callback
+                // Make sure to add this to the Auth0 allowed callback urls !
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
+
+/*
+ * import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AppInsightsService } from '@markpieszak/ng-application-insights';
+import { Auth0DecodedHash, Auth0Error, WebAuth } from 'auth0-js';
+
+import { environment } from '../../environments/environment';
+import { LoginModel } from '../common/models';
+import { SessionStorageService } from '../common/services/session-storage.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthenticationService {
+    private auth0: WebAuth;
+
+    constructor(private sessionStorageService: SessionStorageService, private router: Router, private appInsightsService: AppInsightsService) {
+        this.auth0 = new WebAuth({
+            clientID: environment.clientId,
+            domain: 'stef-heyenrath.eu.auth0.com',
+            responseType: 'token id_token',
+            audience: 'https://qboxnext.web.nl',
+            redirectUri: `${location.origin}/callback`,
+            scope: 'openid'
+        });
+    }
+
+    public login(): void {
+        this.auth0.authorize();
+    }
+
+    public handleAuthentication(): void {
+        this.auth0.parseHash((err: Auth0Error | null, authResult: Auth0DecodedHash) => {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                this.setSession(authResult);
+                this.router.navigate(['/electricity']);
+            } else if (err) {
+                this.router.navigate(['/']);
+                console.log(err);
+                alert(`Error: ${err.error}. Check the console for further details.`);
+            }
+        });
+    }
+
+    private setSession(authResult: Auth0DecodedHash): void {
+        const model = new LoginModel({
+            accessToken: authResult.accessToken,
+            expiresAt: new Date().getTime() + (authResult.expiresIn || 0) * 1000,
+            serialNumber: authResult.idTokenPayload['https://qboxnext.web.nl/SerialNumber']
+        });
+
+        this.appInsightsService.setAuthenticatedUserContext(authResult.idTokenPayload['sub']);
+
+        this.setLoginModel(model);
+    }
+
+    public logout(): void {
+        // Remove from sessionStorage
+        this.clearLoginModel();
+
+        // Go back to the home route
+        this.router.navigate(['']);
+    }
+
+    public getLoginModel(): LoginModel | null {
+        return this.sessionStorageService.get('LoginModel', LoginModel);
+    }
+
+    public clearLoginModel(): void {
+        this.sessionStorageService.clear('LoginModel');
+    }
+
+    public isAuthenticated(): boolean {
+        const model = this.getLoginModel();
+        return model !== null && !model.isExpired();
+    }
+
+    private setLoginModel(loginModel: LoginModel): void {
+        this.sessionStorageService.set('LoginModel', loginModel);
+    }
+}
+*/
---
+++ src-server/WeatherApi/Startup.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WeatherApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // services.AddControllers();
+
+            // Configure AddControllers with AuthorizationPolicyBuilder (stef)
+            services.AddControllers(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            // Auth0
+            services.AddAuth0(options =>
+            {
+                var section = Configuration.GetSection("Auth0Options");
+
+                options.JwtAuthority = section["JwtAuthority"];
+                options.JwtAudience = section["JwtAudience"];
+
+                options.Audience = section["Audience"];
+                options.ClientId = section["ClientId"];
+                options.ClientSecret = section["ClientSecret"];
+                options.Domain = section["Domain"];
+                options.Policies = section.GetSection("Policies").Get<List<string>>();
+            });
+
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //    .AddJwtBearer(options =>
+            //    {
+            //        options.Authority = "https://stef-heyenrath.eu.auth0.com/";
+            //        options.Audience = "https://qboxnext.web.nl";
+            //        options.
+
+            //        //options.
+            //        //options.
+                    
+            //        //options.TokenValidationParameters = new TokenValidationParameters
+            //        //{
+            //        //    NameClaimType = "name"
+            //        //};
+            //    });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(config => 
+            {
+                config.AllowAnyOrigin();
+                config.AllowAnyMethod();
+                config.AllowAnyHeader();
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
