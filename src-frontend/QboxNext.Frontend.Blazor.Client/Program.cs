c4b5028663f4617c7cd4d924d88a16ed362cb019
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-07-03 10:45:05 +0000

    wip

---
+++ src-frontend/QboxNext.Frontend.Blazor.Client/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using QboxNext.Frontend.Blazor.Client.Services;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QboxNext.Frontend.Blazor.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddHttpClient("QboxNext.Frontend.Blazor.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("QboxNext.Frontend.Blazor.ServerAPI"));
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here. For more information, see https://aka.ms/blazor-standalone-auth
+                builder.Configuration.Bind("auth0", options.ProviderOptions);
+
+                // The callback url is : https://localhost:5001/authentication/login-callback
+                // Make sure to add this to the Auth0 allowed callback urls !
+            });
+
+            builder.Services.AddGrpcBearerTokenProvider();
+
+            builder.Services.AddScoped(services =>
+            {
+                // Create a channel with a GrpcWebHandler that is addressed to the backend server. GrpcWebText is used because server streaming requires it.
+                var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+                return GrpcChannel.ForAddress(builder.HostEnvironment.BaseAddress, new GrpcChannelOptions { HttpHandler = httpHandler });
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ src-frontend/QboxNext.Frontend.Blazor.Server/Services/UploadFileService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Fileupload;
+using Grpc.Core;
+
+namespace QboxNext.Frontend.Blazor.Server.Services
+{
+    public class UploadFileService : FileUpload.FileUploadBase
+    {
+        public override async Task<UploadResponse> Upload(UploadRequest request, ServerCallContext context)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            
+            var bytes = request.FileContent;
+
+            return new UploadResponse { FileSize = bytes.Length };
+        }
+    }
+}
