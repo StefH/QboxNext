28781ba182963dc870e3f48372f29ade7f21afb1
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-12-11 20:22:54 +0100

    .

--- src-server/QboxNext.Server.Infrastructure.Azure/Utils/PartitionKeyHelper.cs
+++ src-server/QboxNext.Server.Infrastructure.Azure/Utils/PartitionKeyHelper.cs
@@ -11,6 +11,18 @@ namespace QboxNext.Server.Infrastructure.Azure.Utils
             return $"{serialNumber}:{ConstructDateTimeKey(measureTime)}";
         }
 
+        public static (string SerialNumber, DateTime MeasureTime) Deconstruct(string partitionKey)
+        {
+            var parts = partitionKey.Split(':');
+
+            int value = PartitionKeyStart - int.Parse(parts[1]);
+            int year = value / 10000;
+            int month = (value - year * 10000) / 100;
+            int day = value - year * 10000 - month * 100;
+
+            return (parts[0], new DateTime(year, month, day));
+        }
+
         private static int ConstructDateTimeKey(DateTime measureTime)
         {
             return PartitionKeyStart - measureTime.Year * 10000 - measureTime.Month * 100 - measureTime.Day;
